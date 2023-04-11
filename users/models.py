a2eee6b66468fe3c0175d003961d3a16179fd119
siddarthx07 <siddarthbandi07@gmail.com>
2023-04-11 21:34:20 +0530

    added

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # Delete profile when user is deleted
+    image=models.ImageField(default='default.jpg',upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'    
+
+
+    def save(self):
+        super().save()     
+        img=Image.open(self.image.path)
+
+        if img.height > 300 or img.width>300:
+            output_save=(300,300)
+            img.thumbnail(output_save)
+            img.save(self.image.path)
