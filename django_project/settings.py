75cab8d1e2c822ca7ceefad0a84ff9b9261f5a70
siddarthx07 <siddarthbandi07@gmail.com>
2023-04-21 01:56:00 +0530

    files

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = "/home/Siddarthx07/django_project/blog/static/blog"
+STATIC_URL = '/blog/static/blog' #'/static/'
+STATIC_ROOT = "/home/Siddarthx07/django_project/static"
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
